6f46e44332583254d39230ee26336afd4fe9b4ae
Guilherme Fiuza <guilhermejuca1929@gmail.com>
2026-05-26 09:05:04 -0300

    primeiras rotas da API

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-
 import "express-async-errors"
+
+import { routes } from './routes';
 
 import { errorHandling } from '@/middlewares/error-handling';
 
@@ -8,6 +9,7 @@ import { errorHandling } from '@/middlewares/error-handling';
 const app = express();
 
 app.use(express.json())
+app.use(routes)
 
 app.use(errorHandling)
 
